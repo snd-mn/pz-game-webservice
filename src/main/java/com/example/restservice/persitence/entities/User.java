3beb7dad866d39d9068cff3ec5f7e41e9b09aee1
sir.rollalot <anmu7c4@gmail.com>
2021-03-25 14:00:27 +0100

    docs

--- src/main/java/com/example/restservice/persitence/entities/User.java
+++ src/main/java/com/example/restservice/persitence/entities/User.java
@@ -14,6 +14,7 @@ public class User {
     @GeneratedValue(strategy= GenerationType.AUTO)
     private Long id;
     private String name;
+    @Column(unique=true)
     private String nick;
     @Column(unique=true)
     private String email;
